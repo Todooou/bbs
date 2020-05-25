5ec40f951f89f68a605e60a819e98a18a6c46489
東藤梨里香 <toudouririka@toutoririkanoMacBook-Pro.local>
2020-05-25 17:48:37 +0900

    validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     mount_uploader :picture, PictureUploader
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
+    validates :body, presence: true
   end
