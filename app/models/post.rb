5c669b4f9060db312692146e72ee3e579e72c549
erii todo <eriitodo@toutoririkanoMacBook-Pro.local>
2020-08-22 00:37:49 +0900

    change validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
     mount_uploader :picture, PictureUploader
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
-    validates :body, presence: true
+    validates :body, presence: true, length: { maximum: 1000 }    
+  
   end
